653f118235f2aacd02c93289235b8873eb9878a3
Fernando0211 <fer.rodza@gmail.com>
2021-05-20 15:20:04 -0500

    Activity 4 in python

---
+++ unit_1/act_4.py
@@ -0,0 +1,15 @@
+###Actividad 4###
+
+n1= 0
+n2= 0
+result_add = 0
+result_m = 0
+
+n1=int(input("Enter n1: "))
+n2=int(input("Enter n2: "))
+
+result_add = n1 + n2
+result_m = n1*n2
+
+print("The add is", result_add)
+print("The multiplication is", result_m)
